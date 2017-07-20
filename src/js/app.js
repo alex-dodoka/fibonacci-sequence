6bfc84f4df7400b93f619e1fdd0b67d842071769
alex-dodoka <neksus.92@mail.ru>
2017-07-20 14:02:23 +0300

    The second option is using recursion.

--- src/js/app.js
+++ src/js/app.js
@@ -15,3 +15,26 @@ function fib() {
 function print(arg) {
     $('#print-space').html(arg);
 }
+
+//The second option is using recursion.
+
+function fibRow(x, curVal) {
+
+    let F = function (n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return F(n - 1) + F(n - 2);
+        }
+    };
+
+    curVal = curVal || 1;
+
+    console.log(F(curVal));
+
+    if (x > curVal) {
+        fibRow(x, curVal + 1);
+    }
+}
+
+console.log(fibRow(5));
